0a95791e2a0749d0763641fbcd2ef637c8cf69d6
רינה קלימן < @DESKTOP-PT7G7JA>
2022-11-01 23:01:02 +0200

    ex1

---
+++ Stage0/dotNet5783_8785/Stage0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0} , welcome to my first console application", name);
+            Console.ReadKey();
+        }
+    }
+}
