14c8b1888ee3b03765faf131de51d5eb60b15dcf
CC221b <91909494+CC221b@users.noreply.github.com>
2022-11-04 00:06:09 +0200

    Update Program.cs

--- Stage0/Stage0/Program.cs
+++ Stage0/Stage0/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+//מכיוון שאנו בקבוצה מספר אי זוגי של בנות 
+//והמרצה של הקבוצה אינה מרשה לעשות שלישה
+//קיבלתי הנחיה להגיש לבד את הפרויקט.
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
