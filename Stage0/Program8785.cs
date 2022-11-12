eee2270e06efb2106530bf54c43b6a17b1049826
רינה קלימן < @DESKTOP-PT7G7JA>
2022-11-12 21:46:57 +0200

    ex0

--- Stage0/Program8785.cs
+++ Stage0/Program8785.cs
@@ -1,4 +1,10 @@
-﻿//מכיוון שאנו מספר אי זוגי של בנות ולא ניתנה לי רשות לעשות שלישה, אין לי איך לעשות את רעיון המיזוג שבשלב 0.
+﻿/// <summary>
+///מכיוון שאנו מספר אי זוגי של בנות ואין אפשרות לעשות שלישיה,
+///מרצה הקורס הורה לי לעשות לבד
+///ומכיוון שרציתי לדעת איך עובד מיזוג ולתרגל עליו פתחתי 2 תוכניות ומזגתי עם עצמי
+///8785-ארבעה ספרות אחרונות של הת"ז
+///3252-ארבעה ספרות ראשונות של הת"ז
+/// </summary>
 
 namespace Targil0
 {
@@ -7,14 +13,16 @@ namespace Targil0
         static void Main(string[] args)
         {
             welcome8785();
+            welcome3252();
             Console.ReadKey();
         }
-
+       
         private static void welcome8785()
         {
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
             Console.WriteLine("{0},welcome my first console application", name);
         }
+        static partial void welcome3252();
     }
 }
