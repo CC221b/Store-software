13b74e05e3f3d3dfdf880ed0a103fbe76e8632fb
325288785 <325288785@LAB44PC19>
2022-11-16 17:46:03 +0200

    ex2

---
+++ DalFacade/DalApi/Exceptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi
+{
+    public class ExceptionExists : Exception
+    {
+        public override string Message => "Error: Item already exists.";
+    }
+    public class ExceptionNotExists : Exception
+    {
+        public override string Message => "Error: Item does not exist.";
+    }
+}
---
+++ DalFacade/DalApi/IOrderItem.cs
@@ -0,0 +1,16 @@
+﻿using DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi
+{
+    public interface IOrderItem: ICrud<OrderItem>
+    {
+        public List<OrderItem> ReadByOrderID(int id);
+        public OrderItem ReadByProductIDAndOrderID(int productId, int orderId);
+
+    }
+}
