54f383105502affb6b5e85a9446bb73bc5544709
רינה קלימן < @DESKTOP-PT7G7JA>
2022-11-22 19:39:14 +0200

    finish BlProduct in Stage2

--- BL/BO/Exceptions.cs
+++ BL/BO/Exceptions.cs
@@ -7,3 +7,13 @@ public class ExceptionFromDal : Exception
     public ExceptionFromDal(Exception ex) : base("Exception in Dal.", ex) { }
     public override string Message => "Exception in Dal.";
 }
+
+public class ExceptionInvalidData : Exception
+{
+    public override string Message => "Error: There is invalid data, please enter valid data.";
+}
+
+public class ExceptionExistsInOrder : Exception
+{
+    public override string Message => "Error: Item already exists in the order.";
+}
--- BL/BlImplementation/BlProduct.cs
+++ BL/BlImplementation/BlProduct.cs
@@ -1,16 +1,15 @@
 ﻿using BlApi;
-using BO;
 namespace BlImplementation;
 
 
 internal class BlProduct : IProduct
 {
-    DalApi.IDal IDal = new Dal.DalList();
-    public IEnumerable<ProductForList> GetListProducts()
+    DalApi.IDal Dal = new Dal.DalList();
+    public IEnumerable<BO.ProductForList> GetListProducts()
     {
-        IEnumerable<DO.Product> ListProducts = IDal.Product.GetAll();
-        List<ProductForList> ListProductsForList = new List<ProductForList>();
-        ProductForList productForList = new ProductForList();
+        IEnumerable<DO.Product> ListProducts = Dal.Product.GetAll();
+        List<BO.ProductForList> ListProductsForList = new List<BO.ProductForList>();
+        BO.ProductForList productForList = new BO.ProductForList();
         foreach (var item in ListProducts)
         {
             productForList.ID = item.ID;
@@ -21,11 +20,11 @@ internal class BlProduct : IProduct
         }
         return ListProductsForList;
     }
-    public IEnumerable<ProductItem> GetCatalog()
+    public IEnumerable<BO.ProductItem> GetCatalog()
     {
-        IEnumerable<DO.Product> ListProducts = IDal.Product.GetAll();
-        List<ProductItem> ListProductItem = new List<ProductItem>();
-        ProductItem listProductItem = new ProductItem();
+        IEnumerable<DO.Product> ListProducts = Dal.Product.GetAll();
+        List<BO.ProductItem> ListProductItem = new List<BO.ProductItem>();
+        BO.ProductItem listProductItem = new BO.ProductItem();
         foreach (var item in ListProducts)
         {
             listProductItem.ID = item.ID;
@@ -33,20 +32,20 @@ internal class BlProduct : IProduct
             listProductItem.Price = item.Price;
             listProductItem.Category = (BO.Categories)item.Category;
             listProductItem.Amount = 0;
-            listProductItem.InStock = item.InStock > 0 ? true : false ;
+            listProductItem.InStock = item.InStock > 0 ? true : false;
             ListProductItem.Add(listProductItem);
         }
         return ListProductItem;
     }
-    public Product GetProduct(int id)
-    { 
+    public BO.Product GetProduct(int id)
+    {
         try
         {
-            Product productTypeBO = new Product();
+            BO.Product productTypeBO = new BO.Product();
             if (id > 0)
             {
                 DO.Product productTypeDO = new DO.Product();
-                productTypeDO = IDal.Product.Get(id);
+                productTypeDO = Dal.Product.Get(id);
                 productTypeBO.ID = productTypeDO.ID;
                 productTypeBO.Name = productTypeDO.Name;
                 productTypeBO.Price = productTypeDO.Price;
@@ -57,22 +56,80 @@ internal class BlProduct : IProduct
         }
         catch (Exception ex)
         {
-            throw new ExceptionFromDal(ex);
+            throw new BO.ExceptionFromDal(ex);
         }
     }
-    public void AddProduct(Product product)
+    public void AddProduct(BO.Product product)
     {
-        if (true)
+        try
         {
-
+            if (product.ID > 0 && product.Name != "" && product.Price > 0 && product.InStock > 0)
+            {
+                DO.Product product1 = new DO.Product();
+                product1.ID = product.ID;
+                product1.Name = product.Name;
+                product1.Price = product.Price;
+                product1.InStock = product.InStock;
+                product1.Category = (DO.Categories)product.Category;
+                try
+                {
+                    Dal.Product.Add(product1);
+                }
+                catch (Exception ex)
+                {
+                    throw new BO.ExceptionFromDal(ex);
+                }
+            }
         }
-    }
-    public void UpdateProduct(Product product)
-    {
-
+        catch (Exception)
+        {
+            throw new BO.ExceptionInvalidData();
+        }
     }
     public void DeleteProduct(int id)
     {
-
+        IEnumerable<DO.OrderItem> orderItems = Dal.OrderItem.GetAll();
+        foreach (DO.OrderItem orderItem in orderItems)
+        {
+            if (orderItem.ProductId == id)
+            {
+                throw new BO.ExceptionExistsInOrder();
+            }
+        }
+        try
+        {
+            Dal.Product.Delete(id);
+        }
+        catch (Exception ex)
+        {
+            throw new BO.ExceptionFromDal(ex);
+        }
+    }
+    public void UpdateProduct(BO.Product product)
+    {
+        try
+        {
+            if (product.ID > 0 && product.Name != "" && product.Price > 0 && product.InStock > 0)
+            {
+                try
+                {
+                    DO.Product product1= new DO.Product();
+                    product1 = Dal.Product.Get(product.ID);
+                    product1.Name = product.Name;
+                    product1.Price = product.Price;
+                    product1.InStock = product.InStock;
+                    product1.Category =(DO.Categories)product.Category;
+                    Dal.Product.Update(product1);
+                }
+                catch (Exception ex)
+                {
+                    throw new BO.ExceptionFromDal(ex);
+                }  
+            }        
+        }
+        catch (Exception)
+        {
+            throw new BO.ExceptionInvalidData();
+        }
     }
 }
