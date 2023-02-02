687a66d00985adf75854203a2b76bcdcf1213a5d
רינה קלימן < @DESKTOP-PT7G7JA>
2023-02-03 00:52:41 +0200

    03.02.23 00:52 Stage6

--- DalXml/Product.cs
+++ DalXml/Product.cs
@@ -4,6 +4,7 @@ using DO;
 using System.Security.Cryptography;
 using System.Xml;
 using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 internal class Product : IProduct
 {
@@ -13,31 +14,57 @@ internal class Product : IProduct
         xElement.Add(product);
         return product.ID;
     }
+
     public void Delete(int id)
     {
 
     }
+
     public void Update(DO.Product product)
     {
 
     }
+
     public DO.Product Get(int ID)
     {
-        return new DO.Product();
+        XElement? product = XElement.Load(@"../xml/Products.xml").Descendants("Product")
+            .Where(product => Convert.ToInt32(product?.Element("ID")?.Value) == ID).FirstOrDefault();
+        if (product != null)
+        {
+            return new DO.Product()
+            {
+                ID = Convert.ToInt32(product?.Element("ID")?.Value),
+                Name = product?.Element("Name")?.Value,
+                Price = Convert.ToInt32(product?.Element("Price")?.Value),
+                Category = DO.Categories.TryParse(product?.Element("Category")?.Value, out DO.Categories category) ? (DO.Categories)category : 0,
+                InStock = Convert.ToInt32(product?.Element("InStock")?.Value)
+            };
+        }
+        throw new ExceptionNotExists();
     }
+
     public DO.Product Get(Predicate<DO.Product> func)
     {
-        return new DO.Product();
+        DO.Product product = GetAll().ToList().Find(func);
+        return product.ID != 0 ? product : throw new ExceptionNotExists();
     }
+
     public IEnumerable<DO.Product> GetAll(Func<DO.Product, bool>? func = null)
     {
-        return XElement.Load(@"../xml/Products.xml").Descendants("Product").Select(product => new DO.Product()
+        if (XElement.Load(@"../xml/Products.xml").Descendants("Product").Count() == 0)
         {
-            ID = Convert.ToInt32(product.Element("ID").Value),
-            Name = product.Element("Name").Value,
-            Price = Convert.ToInt32(product.Element("Price").Value),
-            Category = 0,
-            InStock = Convert.ToInt32(product.Element("InStock").Value)
-        });
+            throw new ExceptionEmpty();
+        }
+        IEnumerable<DO.Product> products = from product in XElement.Load(@"../xml/Products.xml").Descendants("Product")
+                                           let category = DO.Categories.TryParse(product?.Element("Category")?.Value, out DO.Categories category) ? (DO.Categories)category : 0
+                                           select new DO.Product()
+                                           {
+                                               ID = Convert.ToInt32(product?.Element("ID")?.Value),
+                                               Name = product?.Element("Name")?.Value,
+                                               Price = Convert.ToInt32(product?.Element("Price")?.Value),
+                                               Category = category,
+                                               InStock = Convert.ToInt32(product?.Element("InStock")?.Value)
+                                           };
+        return (func == null) ? products : products.Where(func);
     }
 }
