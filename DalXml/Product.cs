2a9c80255567d63c8e6aa831651fa18e688a636f
רינה קלימן < @DESKTOP-PT7G7JA>
2023-02-13 20:33:41 +0200

    13/02/2023 20:33

--- DalXml/Product.cs
+++ DalXml/Product.cs
@@ -8,24 +8,23 @@ using static System.Net.Mime.MediaTypeNames;
 
 internal class Product : IProduct
 {
+    public class XmlElement
+    {
+        public XElement? productRoot { get; set; }
+        public string Path { get; set; } = @"../xml/Products.xml";
+    }
+
+    public XmlElement? xmlElement { get; set; }
+
     public int Add(DO.Product product)
     {
-        //IEnumerable<XmlElement> doc = XElement.Load(@"../xml/Products.xml");
-        //XmlElement? root = doc.DocumentElement;
-        //XmlElement Product = doc.CreateElement("Product");
-        //XmlElement Name = doc.CreateElement("Name");
-        //XmlElement Price = doc.CreateElement("Price");
-        //XmlElement ID = doc.CreateElement("ID");
-        //XmlElement InStock = doc.CreateElement("InStock");
-        //ID.InnerText = product.ID.ToString();
-        //Name.InnerText = product.Name;
-        //Price.InnerText = product.Price.ToString();
-        //InStock.InnerText = product.InStock.ToString();
-        //Product.AppendChild(Name);
-        //Product.AppendChild(ID);
-        //Product.AppendChild(Price);
-        //Product.AppendChild(InStock);
-        //root.AppendChild(Product);
+        XElement id = new XElement("ID", product.ID);
+        XElement name = new XElement("Name", product.Name);
+        XElement price = new XElement("Price", product.Price);
+        XElement inStock = new XElement("name", product.InStock);
+        XElement student = new XElement("student", id, name, price, inStock);
+        xmlElement?.productRoot?.Add(student);
+        xmlElement?.productRoot?.Save(xmlElement.Path);
         return product.ID;
     }
 
